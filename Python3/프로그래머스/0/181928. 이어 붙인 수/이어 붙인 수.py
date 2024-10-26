f52754cd5a507136baa40cac2e04307c6da28923
Eden Min Kim <kmin91143@gmail.com>
2024-10-27 02:31:19 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,7 @@
+def solution(num_list):
+    odd, even = '', ''
+    for num in num_list:
+        if (num%2 == 1): odd += str(num)
+        else: even += str(num)
+    answer = int(odd) + int(even)
+    return answer

4f6d50493bcfaecf8b57380a711a779479ed05c8
Eden Min Kim <kmin91143@gmail.com>
2024-11-02 02:21:01 +0900

    [level 0] Title: 배열 만들기 6, Time: 132.99 ms, Memory: 18.1 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,8 @@
+def solution(arr):
+    answer = []
+    for i in range(len(arr)):
+        if answer == []: answer.append(arr[i])
+        elif answer[-1] == arr[i]: answer.pop()
+        else : answer.append(arr[i])
+    if answer == []: answer = [-1]
+    return answer

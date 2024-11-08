ef90bf5b3e23c662f42dac03795e995f724a458f
Eden Min Kim <kmin91143@gmail.com>
2024-11-08 16:50:37 +0900

    [level 0] Title: 모스부호 (1), Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.py
@@ -0,0 +1,11 @@
+codes = {'a':".-",'b':"-...",'c':"-.-.",'d':"-..",'e':".",'f':"..-.",'g':"--.",'h':"....",'i':"..",'j':".---",'k':"-.-",'l':".-..",'m':"--",'n':"-.",'o':"---",'p':".--.",'q':"--.-",'r':".-.",'s':"...",'t':"-",'u':"..-",'v':"...-",'w':".--",'x':"-..-",'y':"-.--",'z':"--.."}
+
+morse = dict()
+for k, v in codes.items(): morse[v] = k
+
+def solution(letter):
+    answer = ''
+    encrypted = letter.split(" ")
+    for elem in encrypted:
+        answer += morse[elem]
+    return answer

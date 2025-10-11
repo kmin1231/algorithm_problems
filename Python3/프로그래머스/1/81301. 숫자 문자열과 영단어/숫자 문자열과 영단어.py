8498815a49ba68ab3447ed27b52c029153d33f11
Eden Min Kim <kmin91143@gmail.com>
2025-10-11 10:22:05 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 9.38 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,18 @@
+num_words = {
+    "zero": "0",
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+}
+
+def solution(s):
+    for word, digit in num_words.items():
+        s = s.replace(word, digit)
+    answer = int(s)
+    return answer

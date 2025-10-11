1ec574249fcc9162ec27e3574240906105dce400
Eden Min Kim <kmin91143@gmail.com>
2025-10-11 11:06:37 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.33 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,19 @@
+import re
+
+def solution(new_id):
+    new_id = new_id.lower()
+
+    new_id = re.sub('[^a-z0-9\-_.]', '', new_id)
+
+    new_id = re.sub('\.+', '.', new_id)
+
+    new_id = new_id.strip('.')
+
+    if new_id == '': new_id = 'a'
+
+    new_id = new_id[:15].rstrip('.')
+
+    while len(new_id) < 3:
+        new_id += new_id[-1]
+
+    return new_id
